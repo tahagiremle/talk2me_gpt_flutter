5925a2752fc28664c4496c5cf4e1eba060400214
tahagiremle <tahagiremle@gmail.com>
2024-09-09 13:59:04 +0300

    add appbar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -21,75 +21,109 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          elevation: 10,
+          centerTitle: false,
+          backgroundColor: Colors.white,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Image.asset('assets/gpt-robot.png'),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Talk2Me',
+                    style: TextStyle(color: Colors.black),
+                  )
+                ],
+              ),
+              Image.asset(
+                'assets/volume-high.png',
+                color: Colors.blue[700],
+              )
+            ],
+          ),
+        ),
         body: Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: _message.length,
-            itemBuilder: (context, index) {
-              final messege = _message[index];
-              return ListTile(
-                title: Align(
-                  alignment: messege.isUser
-                      ? Alignment.centerRight
-                      : Alignment.centerLeft,
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: messege.isUser
-                            ? Colors.blue[700]
-                            : Colors.grey[300],
-                        borderRadius: messege.isUser
-                            ? BorderRadius.only(
-                                topLeft: Radius.circular(20),
-                                bottomRight: Radius.circular(20),
-                                bottomLeft: Radius.circular(20))
-                            : BorderRadius.only(
-                                topRight: Radius.circular(20),
-                                bottomRight: Radius.circular(20),
-                                bottomLeft: Radius.circular(20))),
-                    child: Text(
-                      messege.text,
-                      style: TextStyle(
-                          color: messege.isUser ? Colors.white : Colors.black),
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _message.length,
+                itemBuilder: (context, index) {
+                  final messege = _message[index];
+                  return ListTile(
+                    title: Align(
+                      alignment: messege.isUser
+                          ? Alignment.centerRight
+                          : Alignment.centerLeft,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: messege.isUser
+                                ? Colors.blue[700]
+                                : Colors.grey[300],
+                            borderRadius: messege.isUser
+                                ? BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20))
+                                : BorderRadius.only(
+                                    topRight: Radius.circular(20),
+                                    bottomRight: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20))),
+                        child: Text(
+                          messege.text,
+                          style: TextStyle(
+                              color:
+                                  messege.isUser ? Colors.white : Colors.black),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.all(16),
-          child: Container(
-            decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(32),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 5,
-                        offset: Offset(0, 3)),
-                  ]),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                        hintText: "Write your message",
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.symmetric(horizontal: 20)),
-                  ),
-                ),
-                SizedBox(width: 8),
-                IconButton(onPressed: () {}, icon: Icon(Icons.send))
-              ],
+                  );
+                },
+              ),
             ),
-          ),
-        ),
-      ],
-    ));
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(32),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 5,
+                          offset: Offset(0, 3)),
+                    ]),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                            hintText: "Write your message",
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20)),
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: GestureDetector(
+                        child: Image.asset('assets/send.png'),
+                        onTap: () {},
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
