164c38a89b4f29c901ac9986d72dc4e4bb00119d
tahagiremle <tahagiremle@gmail.com>
2024-09-09 13:48:15 +0300

    Implement TextField for message input

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _controller = TextEditingController();
   final List<Message> _message = [
     Message(text: "Hi", isUser: true),
     Message(text: "Hello", isUser: false),
@@ -20,36 +21,75 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-      itemCount: _message.length,
-      itemBuilder: (context, index) {
-        final messege = _message[index];
-        return ListTile(
-          title: Align(
-            alignment:
-                messege.isUser ? Alignment.centerRight : Alignment.centerLeft,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  color: messege.isUser ? Colors.blue[700] : Colors.grey[300],
-                  borderRadius: messege.isUser
-                      ? BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                          bottomLeft: Radius.circular(20))
-                      : BorderRadius.only(
-                          topRight: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                          bottomLeft: Radius.circular(20))),
-              child: Text(
-                messege.text,
-                style: TextStyle(
-                    color: messege.isUser ? Colors.white : Colors.black),
-              ),
+        body: Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: _message.length,
+            itemBuilder: (context, index) {
+              final messege = _message[index];
+              return ListTile(
+                title: Align(
+                  alignment: messege.isUser
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: messege.isUser
+                            ? Colors.blue[700]
+                            : Colors.grey[300],
+                        borderRadius: messege.isUser
+                            ? BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20))
+                            : BorderRadius.only(
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20))),
+                    child: Text(
+                      messege.text,
+                      style: TextStyle(
+                          color: messege.isUser ? Colors.white : Colors.black),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Container(
+            decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(32),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 5,
+                        offset: Offset(0, 3)),
+                  ]),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                        hintText: "Write your message",
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20)),
+                  ),
+                ),
+                SizedBox(width: 8),
+                IconButton(onPressed: () {}, icon: Icon(Icons.send))
+              ],
             ),
           ),
-        );
-      },
+        ),
+      ],
     ));
   }
 }
